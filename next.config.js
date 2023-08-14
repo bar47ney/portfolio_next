5f924da447b9d6d9be52358ecc661f1fab739aa8
bar47ney <bar47ney@gmail.com>
2023-08-14 23:29:21 +0300

    add full project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:[
+            "cdn1.ozone.ru",
+            "images.pexels.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
---
+++ src/app/ssr/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SsrPage() {
+    return (
+        <div>
+            SsrPage
+        </div>
+    )
+}
